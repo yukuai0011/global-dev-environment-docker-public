5ebb9f9f0dc9f94bfc1479ea1c613db94b5aea2d
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2024-10-11 14:38:16 +1100

    Remove unused environment variables and simplify setup.py
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ import subprocess
 # Get the SSH private and public key from the environment variable
 SSH_PRIVATE_KEY = os.environ.get("SSH_PRIVATE_KEY")
 SSH_PUBLIC_KEY = os.environ.get("SSH_PUBLIC_KEY")
-GIT_REPOSITORY_SSH_ADDRESS = os.environ.get("GIT_REPOSITORY_SSH_ADDRESS")
 GIT_USER_NAME = os.environ.get("GIT_USER_NAME")
 GIT_USER_EMAIL = os.environ.get("GIT_USER_EMAIL")
-APP_DIRECTORY = os.environ.get("APP_DIRECTORY")
-APP_FILE_NAME = os.environ.get("APP_FILE_NAME")
 
 # Define the file path where the private and public key will be saved
 ssh_private_key_path = os.path.expanduser("~/.ssh/ssh_key")
@@ -69,48 +66,3 @@ for command in commands:
         print(f"Successfully ran: {' '.join(command)}")
     else:
         print(f"Failed to run: {' '.join(command)}")
-
-# Clone the repository
-result = subprocess.run(
-    ["git", "clone", GIT_REPOSITORY_SSH_ADDRESS, APP_DIRECTORY],
-    check=True,
-)
-
-if result.returncode == 0:
-    print(f"Successfully cloned the repository")
-else:
-    print(f"Failed to clone the repository")
-
-# cd into the app directory
-os.chdir(APP_DIRECTORY)
-
-
-# run the app
-
-# if the file is a python file
-if APP_FILE_NAME.endswith(".py"):
-    result = subprocess.run(
-        ["python", f"{APP_FILE_NAME}"],
-        check=True,
-    )
-
-# if the file is a ipynb file
-
-elif APP_FILE_NAME.endswith(".ipynb"):
-    result = subprocess.run(
-        [
-            "jupyter",
-            "nbconvert",
-            "--to",
-            "notebook",
-            "--inplace",
-            "--execute",
-            f"{APP_FILE_NAME}",
-        ],
-        check=True,
-    )
-
-if result.returncode == 0:
-    print(f"Successfully ran the app")
-else:
-    print(f"Failed to run the app")
